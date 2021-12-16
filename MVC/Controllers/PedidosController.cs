28e63b3a7da9341e0eab413efcf934a7de80f0f7
soporteti <soporteti@TIZEBOL>
2021-12-16 13:44:53 -0600

    Agregar archivos de proyecto.

---
+++ MVC/Controllers/PedidosController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+using MVC.Models.ViewModels;
+
+namespace MVC.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // GET: Pedidos
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using(ZEBOLEntities db= new ZEBOLEntities())
+
+
+            {
+                lst = (from d in db.Pedido_Alistar
+                           select new ListTablaViewModel
+                           {
+
+                               Pedido = d.Pedido,
+                               Fecha = (DateTime)d.Fecha,
+                               HoraCreado = d.HoraCreado,
+                               FechaEntrega = (DateTime)d.FechaEntrega,
+                               CodCliente = d.CodCliente,
+                               NombreCliente = d.NombreCliente,
+                               Vendedor = d.Vendedor,
+                               FormaEntrega = d.FormaEntrega,
+                               Almacen = d.Almacen
+
+
+                           }).ToList();
+            } 
+
+            return View(lst);
+        }
+    }
+}
---
+++ MVC/Models/ViewModels/ListTablaViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models.ViewModels
+{
+    public class ListTablaViewModel
+    {
+
+        public int Pedido { get; set; }
+        public DateTime Fecha { get; set; }
+        public string HoraCreado { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public string CodCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public string Vendedor { get; set; }
+        public string FormaEntrega { get; set; }
+        public string Almacen { get; set; }
+
+
+
+
+    }
+}
